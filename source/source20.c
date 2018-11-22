5710bca96b30d7a180a03ed9c785e01c8dd571ab
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2018-11-22 20:58:11 +0530

    Create source20.c

---
+++ source/source20.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char ch = 'A';
+    switch(ch)
+    {
+
+        case 'A'  : printf("Case label is A");
+        case 'B'  : printf("Case label is B");
+
+    }
+        return 0;
+}
+
