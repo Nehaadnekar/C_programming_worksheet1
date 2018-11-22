bab6f8e6d49f3775a7906d68775a424485c729ec
Neha Adnekar <43112829+Nehaadnekar@users.noreply.github.com>
2018-11-22 18:41:02 +0530

    Create source6.c

---
+++ source/source6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+        int a =  0;
+        if(a=0) 
+        {
+             printf("hello");
+        }
+        else
+        {
+
+             printf("world");
+        }
+        return 0;
+}
+
